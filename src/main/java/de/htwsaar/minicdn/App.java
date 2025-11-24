dfd5d1be680e95f1f03ff87c7c882bf9fb730418
Michał Roziel <rozielmichal@gmail.com>
2025-11-25 00:38:45 +0100

    ✨ Initiating maven project & .gitignore

---
+++ src/main/java/de/htwsaar/minicdn/App.java
@@ -0,0 +1,13 @@
+package de.htwsaar.minicdn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
