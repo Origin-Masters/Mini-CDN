0c5c1456bd901b1fb163b378582c65d580fceb11
Valentin S. <akirpap03@gmail.com>
2025-12-20 18:03:26 +0100

    TS-M5: Dependencys von Spring web & picocli in der pom hinzugefügt, basic health check implementiert

--- src/main/java/de/htwsaar/minicdn/App.java
+++ src/main/java/de/htwsaar/minicdn/App.java
@@ -1,12 +1,15 @@
 package de.htwsaar.minicdn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
