2747fddd0731b080bf26fa776a50e731ddfd9ab7
Valentin S. <akirpap03@gmail.com>
2026-03-11 14:10:32 +0100

    start over jar

---
+++ cli/src/main/java/de/htwsaar/minicdn/cli/service/system/ServiceLauncher.java
@@ -0,0 +1,21 @@
+package de.htwsaar.minicdn.cli.service.system;
+
+import java.nio.file.Path;
+
+/**
+ * Adapter-Port zum Starten lokaler Service-Prozesse.
+ *
+ * <p>Die fachliche Startlogik kennt nur dieses Interface und bleibt damit
+ * von der konkreten Prozess-/Transport-Bindung entkoppelt.
+ */
+public interface ServiceLauncher {
+
+    /**
+     * Startet einen Service-Prozess.
+     *
+     * @param jarPath Pfad zur ausführbaren JAR
+     * @param springProfile aktives Spring-Profil
+     * @param logPath Ziel-Datei für stdout/stderr
+     */
+    void start(Path jarPath, String springProfile, Path logPath);
+}
