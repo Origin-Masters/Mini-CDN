c538dbeee3af1b0de471e1b57a92c8916a6271a6
Xudong <inkzxd@gmail.com>
2026-03-14 17:53:31 +0800

    expand CLI service tests

---
+++ cli/src/test/java/de/htwsaar/minicdn/cli/service/admin/AdminUserServiceTest.java
@@ -0,0 +1,24 @@
+package de.htwsaar.minicdn.cli.service.admin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminUserServiceTest {
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void listUsersRaw() {
+    }
+
+    @Test
+    void parseUsers() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
