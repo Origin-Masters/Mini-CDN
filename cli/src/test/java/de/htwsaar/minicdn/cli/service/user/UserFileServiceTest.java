221cd72756b30ee98634861eb2a5a563a187b3fb
Xudong <inkzxd@gmail.com>
2026-03-14 17:53:31 +0800

    expand CLI service tests

---
+++ cli/src/test/java/de/htwsaar/minicdn/cli/service/user/UserFileServiceTest.java
@@ -0,0 +1,12 @@
+package de.htwsaar.minicdn.cli.service.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserFileServiceTest {
+
+    @Test
+    void downloadViaRouter() {
+    }
+}
